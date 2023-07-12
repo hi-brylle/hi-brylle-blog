29fafcb38e168aa782643f721e2e7be8ddeda36c
Brylle <kiirbritmiin@gmail.com>
2023-07-12 19:17:37 +0800

    Create dummy page for architecting server-side push using SSEs

---
+++ src/routes/serial-port-sveltekit-sse/+page.svelte
@@ -0,0 +1,40 @@
+<main>
+    <h1>
+        Architecting server-side push for Node SerialPort in SvelteKit using Server-Sent Events
+    </h1>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nemo incidunt sapiente ipsa culpa ex illum molestiae? Repellat, culpa? Dignissimos incidunt pariatur deserunt necessitatibus possimus eveniet maiores expedita reprehenderit soluta?
+        Distinctio odit consectetur at dolorem, exercitationem voluptates assumenda illum sunt repudiandae nobis facilis? Accusantium quia nisi, dicta officiis expedita saepe vitae sint commodi itaque ut beatae ea eaque. Placeat, corrupti.
+        Laudantium dicta ipsum unde veritatis magni doloremque dolores fuga error sapiente suscipit corrupti deserunt, molestias atque maxime repellat adipisci, similique, laboriosam molestiae fugit cupiditate. Blanditiis a delectus odio assumenda quasi?
+        Doloribus, corporis mollitia vel sit facere quas excepturi exercitationem aut quidem perspiciatis debitis blanditiis neque. Possimus facilis exercitationem laudantium eveniet sit. Dolorem aut consectetur adipisci nemo, reiciendis modi cumque necessitatibus.
+        Optio nihil aut quod officia repudiandae vitae, obcaecati ex. Hic asperiores eius aspernatur unde vitae facere consectetur minima earum architecto doloremque? Nesciunt labore animi possimus porro reiciendis. A, dolores voluptate!
+        Dolorem iusto quam autem corporis dolore necessitatibus, rem harum ipsam nulla atque nemo, porro iure cum dolores aliquid eius perspiciatis ex. Nam exercitationem in consequuntur sint repellat totam consectetur officia.
+        Inventore totam praesentium deleniti suscipit eius excepturi quam quod sequi, incidunt perferendis tenetur velit, dolor libero officia voluptates temporibus soluta animi aut possimus cum quis, cumque quos. Beatae, deleniti nam.
+        Excepturi maiores distinctio voluptates odit, libero velit dolor doloribus! Explicabo doloremque nihil in? Iste ducimus, ipsum quas, expedita dolores enim unde, similique magnam adipisci culpa hic dolorum nesciunt cupiditate modi?
+        Accusamus, labore error eum molestiae corporis quasi odit eaque nesciunt obcaecati! Recusandae impedit eos magnam! Saepe, voluptates eligendi fugiat nostrum exercitationem, quo vero, assumenda labore totam neque quis inventore voluptate.
+        Odio nihil, porro cumque odit ullam distinctio labore sequi cum. Quas tenetur enim vero iure, corrupti reprehenderit tempora eum quam beatae adipisci vel distinctio fugit, doloribus numquam. Laboriosam, odio neque.
+        Asperiores, tenetur laboriosam quaerat sapiente deserunt, tempore ea perspiciatis optio voluptatem sit autem aut mollitia repellendus corporis architecto distinctio quae. Provident, quae? Aliquid ullam praesentium quae minus, explicabo consequuntur officia?
+        Quasi modi debitis aut dolorem ipsum quisquam quos earum ipsa animi rerum omnis, voluptate numquam sint alias possimus placeat. Rem necessitatibus velit asperiores iure enim omnis dolorum saepe blanditiis nisi?
+        Reiciendis nobis delectus non ad, consectetur rem veniam culpa nulla temporibus voluptatibus a ab sint necessitatibus officiis! Beatae, explicabo vel. Et sit molestias quia ducimus porro quasi ab at reprehenderit.
+        Expedita neque est asperiores reprehenderit illum quos eius autem fugiat ipsa repudiandae sed voluptate, blanditiis voluptates exercitationem cumque incidunt mollitia esse enim! Corrupti fugit dolor veniam similique, ipsa nemo atque.
+        Cum debitis, fuga voluptatem quisquam pariatur porro enim maiores, tempore possimus deserunt eos, nisi ratione obcaecati nam sint natus non magnam deleniti necessitatibus autem itaque nihil? Atque corporis voluptatem nisi.
+        Architecto assumenda asperiores quis similique. Odit, incidunt excepturi aspernatur sed reiciendis ipsam labore nihil illo porro quisquam quis quaerat recusandae possimus tempora laboriosam dignissimos aperiam ea eligendi vero quo consequuntur?
+        Laborum, dolore error iure expedita numquam nesciunt praesentium unde alias ipsam cupiditate? Reiciendis magnam unde iste quod sit inventore earum saepe veniam non, minima voluptatem praesentium eius quo fuga tempore.
+        Necessitatibus totam velit reprehenderit mollitia modi, inventore soluta sunt illo ullam est, repudiandae aspernatur placeat consectetur ipsum aut suscipit asperiores. Excepturi in rem adipisci recusandae! Recusandae illum eum quo autem!
+        Impedit vel delectus excepturi, aperiam in architecto quisquam libero nulla distinctio corrupti reprehenderit magnam temporibus a voluptatem enim eum est debitis sit. Officiis quo voluptates recusandae ipsum eius pariatur quam.
+        Reprehenderit, praesentium ab deleniti doloribus voluptatem ut soluta, eos hic molestias quaerat omnis possimus qui perferendis provident tenetur eveniet voluptate? Molestias saepe autem a incidunt vel rerum, similique quidem laborum.
+    </p>
+</main>
+
+<style>
+    main {
+        padding: 50px 100px 50px 100px;
+        color: #CED4E3;
+    }
+
+    p {
+        text-align: justify;
+        text-justify: inter-word;
+    }
+</style>
