0d3dee4a3609c6a869117ccf8158d9c851b99811
Brylle <kiirbritmiin@gmail.com>
2023-07-28 18:02:44 +0800

    Change url of app side server push entry

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
         {url: "/about-this-blog", title: "What this blog is for", selected: false},
         {url: "/about-me", title: "About me", selected: false},
         {url: "/serial-port-sveltekit-sse", title: "Architecting server-side push for Node SerialPort in SvelteKit using Server-Sent Events", selected: false},
-        {url: "/general-sveltekit-sse", title: "App-wide server-side push in SvelteKit using Server-Sent Events", selected: false},
+        {url: "/app-wide-sveltekit-sse", title: "App-wide server-side push in SvelteKit using Server-Sent Events", selected: false},
     ]
 
     $: {
