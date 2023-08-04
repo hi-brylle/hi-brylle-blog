31a0d1d3a6d0be80deecbec1e1355dadf6ad0b17
Brylle <kiirbritmiin@gmail.com>
2023-08-05 01:53:09 +0800

    Clean up code

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	// server: {
-	// 	fs: {
-	// 		allow: [
-	// 			searchForWorkspaceRoot(process.cwd()),
-	// 			"/downloads/resume.pdf"
-	// 		]
-	// 	}
-	// }
+	plugins: [sveltekit()]
 });
