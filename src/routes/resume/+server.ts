27c1c10f60a817a353e8fd715ee478912c4d0808
Brylle <kiirbritmiin@gmail.com>
2023-08-04 17:58:55 +0800

    Add resume endpoint

--- src/routes/resume/+server.ts
+++ src/routes/resume/+server.ts
@@ -1,11 +1,12 @@
+import fs from 'fs'
 import type { RequestHandler } from "@sveltejs/kit"
 
 export const GET = (async ({ request }) => {
-    
-    return new Response(null, {
+    const pdf = fs.readFileSync("./resume/resume.pdf")
+    return new Response(pdf, {
         headers: {
             "Content-type" : "application/pdf",
-            "Content-Disposition": "attachment; filename=resume.pdf"
+            "Content-Disposition": "attachment; filename=resume-viernes.pdf"
         }
     })
 }) satisfies RequestHandler
