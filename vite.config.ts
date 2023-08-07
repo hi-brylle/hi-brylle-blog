c0e07189d37885c8eaa6ed4f3fd7681af68c0f07
Brylle <kiirbritmiin@gmail.com>
2023-08-08 04:57:11 +0800

    Export local server for mobile testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: true
+	}
 });
