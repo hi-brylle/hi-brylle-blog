59a1317f576eb4697651403a30e5956bfdaddf1a
Brylle <kiirbritmiin@gmail.com>
2023-08-14 01:18:54 +0800

    Use generics to infer nav links type in link store

--- src/lib/link-store.ts
+++ src/lib/link-store.ts
@@ -1,4 +1,4 @@
-import { derived, writable } from "svelte/store";
+import { derived, writable, type Writable } from "svelte/store";
 
 export const nav_links = writable([
     {url: "/about-this-blog", title: "What this blog is for", selected: false},
@@ -7,9 +7,9 @@ export const nav_links = writable([
     {url: "/app-wide-sveltekit-sse", title: "App-wide server-side push in SvelteKit using Server-Sent Events", selected: false},
 ])
 
-export const links = derived(nav_links, (values: { url: string; title: string; selected: boolean; }[]) => {    
+export const links = derived(nav_links, (values: (typeof nav_links) extends Writable<infer T> ? T : never) => {    
     let map = new Map<string, string>()
-    values.map((value: { url: string; title: string; selected: boolean; }) => {
+    values.map((value: (typeof values) extends (infer T)[] ? T : never) => {
         map.set(value.url, value.title)
     })
     return map
