26c0e5da7af35d2ebc543574402ecb25eb5b8ec6
Brylle <kiirbritmiin@gmail.com>
2023-12-05 23:43:19 +0800

    Simplify landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,49 @@
 </script>
 
 <div>
-    {#each $nav_links as link}
-        <a
-            href={link.url}
-            class:selected={link.selected}>
-            {link.title}
-        </a>
-    {/each}
+    <ul>
+        {#each $nav_links as link}
+            <li>
+            <h1>
+            <a
+                href={link.url}
+                class:selected={link.selected}>
+                {link.title}
+            </a>
+            </h1>
+            </li>
+        {/each}
+    </ul>
 </div>
+
+<style>
+
+    div {
+        height: 100vh;
+        background-color: #3D625B;
+    }
+
+    ul {
+        margin: 0;
+        padding: 5%;
+        list-style-type: none;
+    }
+
+    li {
+        margin-bottom: 5%;
+    }
+
+    h1 {
+        padding: 0;
+        margin: 0;
+    }
+
+    a {
+        color: #CED4E3;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: black;
+    }
+</style>
