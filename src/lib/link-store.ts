73fd2c7ee06ce0c19d1d2cdf82a34ac4d034bba7
Brylle <kiirbritmiin@gmail.com>
2025-02-16 11:44:35 +0800

    Add estimate read time

--- src/lib/link-store.ts
+++ src/lib/link-store.ts
@@ -4,7 +4,7 @@ import { derived, writable, type Writable } from "svelte/store";
 export const nav_links = writable([
     {url: "/app-wide-sveltekit-sse", title: "App-wide server-side push in SvelteKit using Server-Sent Events", date_written: "28 July 2023", est_read_time: 4},
     {url: "/serial-port-sveltekit-sse", title: "Architecting server-side push for Node SerialPort in SvelteKit using Server-Sent Events", date_written: "13 July 2023", est_read_time: 5},
-    {url: "/petri-net-type-systems-correspondence", title: "Finding correspondences between Petri net constructs and type system features", date_written: "15 February 2025", est_read_time: 0}
+    {url: "/petri-net-type-systems-correspondence", title: "Finding correspondences between Petri net constructs and type system features", date_written: "15 February 2025", est_read_time: 18}
 ])
 
 export const links = derived(nav_links, (values: (typeof nav_links) extends Writable<infer T> ? T : never) => {    
