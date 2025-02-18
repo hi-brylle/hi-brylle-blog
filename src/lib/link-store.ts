dfa5f4b2ec8d1eec1148a00ac9d8ace99a88844d
Brylle <kiirbritmiin@gmail.com>
2025-02-18 20:13:53 +0800

    Reorder links in home page

--- src/lib/link-store.ts
+++ src/lib/link-store.ts
@@ -2,9 +2,9 @@ import { derived, writable, type Writable } from "svelte/store";
 
 // Add navigation links here for every entry.
 export const nav_links = writable([
+    {url: "/petri-net-type-systems-correspondence/", title: "Finding correspondences between Petri net constructs and type system features", date_written: "15 February 2025", est_read_time: 18},
     {url: "/app-wide-sveltekit-sse/", title: "App-wide server-side push in SvelteKit using Server-Sent Events", date_written: "28 July 2023", est_read_time: 4},
     {url: "/serial-port-sveltekit-sse/", title: "Architecting server-side push for Node SerialPort in SvelteKit using Server-Sent Events", date_written: "13 July 2023", est_read_time: 5},
-    {url: "/petri-net-type-systems-correspondence/", title: "Finding correspondences between Petri net constructs and type system features", date_written: "15 February 2025", est_read_time: 18}
 ])
 
 export const links = derived(nav_links, (values: (typeof nav_links) extends Writable<infer T> ? T : never) => {    
